6070db98a1969c9068eec504f2cca59bf66c32b7
Sumit Sharma <sumit.sharma@bda.media>
2019-12-13 14:31:07 +0530

    Update ignor file and crawler file andf function

--- app/Http/Controllers/Crawler.php
+++ app/Http/Controllers/Crawler.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Goutte\Client;
+use App\Result;
 
 class Crawler extends Controller
 {
@@ -12,4 +13,15 @@ class Crawler extends Controller
     public function index() {
     	return "Not found";
     }
+
+    public function getCurrentResult(){
+
+
+    }
+
+    public function getOldResult(){
+
+
+    }
+
 }
