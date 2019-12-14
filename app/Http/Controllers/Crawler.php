a34b1ffd056988a9a1ed529a312b71ba61454b89
Kuldeepbda <kuldeep.raj@bda.media>
2019-12-14 11:14:51 +0530

    New changes html

--- app/Http/Controllers/Crawler.php
+++ app/Http/Controllers/Crawler.php
@@ -11,25 +11,26 @@ class Crawler extends Controller
 {
     //
     public function index() {
-    	return "Not found";
+    	return view('index');
     }
 
     public function getCurrentResult(){
-
-        $url = "https://xosodaiphat.com/xsmb-xo-so-mien-bac.html";
+    	return view('currentResult');
+        /*$url = "https://xosodaiphat.com/xsmb-xo-so-mien-bac.html";
         $resultData = crawlUrl($url);
         echo "<pre>";
         print_r($resultData);
-        exit();
+        exit();*/
     }
 
     //Get old result from database are per required
     public function getOldResult() {
-    	$url = "https://xosodaiphat.com/xsmb-xo-so-mien-bac.html";
+    	/*$url = "https://xosodaiphat.com/xsmb-xo-so-mien-bac.html";
         $resultData = crawlUrlFirst($url);
         echo "<pre>";
         print_r($resultData);
-        exit();
+        exit();*/
+        return view('oldResult');
     }
 
 }
