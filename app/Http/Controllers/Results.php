cf93d32734f1320debbaf601ec1e4f067231ddfc
Sumit Sharma <sumit.sharma@bda.media>
2019-12-14 13:16:14 +0530

    Update result page with live data

--- app/Http/Controllers/Results.php
+++ app/Http/Controllers/Results.php
@@ -7,24 +7,10 @@ use Illuminate\Http\Request;
 class Results extends Controller
 {
     public function index(){
-        $result = new Result();
-        $result = Result::all();
-        echo "<pre>";
-        print_r($result);
-        return $result;
-       /* $result->lottery_region = 'Northern';
-        $result->lottery_company = 'SXMB/XSMB';
-        $result->prize_1 = '[ { "numbers": 18 } , { "prize_amount": 9 } ]';
-        $result->prize_2 = '[ { "numbers": 82 } , { "prize_amount": 9 } ]';
-        $result->prize_3 = '[ { "numbers": 81 } , { "prize_amount": 9 } ]';
-        $result->prize_4 = '[ { "numbers": 58 } , { "prize_amount": 9 } ]';
-        $result->prize_5 = '[ { "numbers": 68 } , { "prize_amount": 9 } ]';
-        $result->prize_6 = '[ { "numbers": 8 } , { "prize_amount": 9 } ]';
-        $result->prize_7 = '[ { "numbers": 8 } , { "prize_amount": 9 } ]';
-        $result->prize_8 = '[ { "numbers": 8 } , { "prize_amount": 9 } ]';
-        $result->prize_9 = '[ { "numbers": 8 } , { "prize_amount": 9 } ]';
-        $result->result_day_time = time();
-        $result->save();*/
+        $result = Result::orderBy('created_at', 'desc')->get();
+        $data['content'] = $result;
+        //return view('currentResult',$data)->render();
+        return view('currentResult')->with($data);
     }
 
     public function show(Request $request,$region){
