0598e8bcfc4c604e6d418661c774506a309633e5
Kuldeepbda <kuldeep.raj@bda.media>
2019-12-16 10:11:50 +0530

    create two menu and pages

--- app/Http/Controllers/Results.php
+++ app/Http/Controllers/Results.php
@@ -18,4 +18,29 @@ class Results extends Controller
 
         return $result;
     }
+
+
+    public function xsmn(){
+        $result = Result::where('lottery_region', 'xsmn')->orderBy('created_at', 'desc')->get();
+        $data['content'] = $result;
+        //return view('currentResult',$data)->render();
+        return view('xsmnResult')->with($data);
+    }
+
+    public function xsmnShow(Request $request,$region){
+        $result = Result::where('lottery_region',$region)->get();
+        return $result;
+    }
+
+    public function xsmt(){
+        $result = Result::where('lottery_region', 'xsmt')->orderBy('created_at', 'desc')->get();
+        $data['content'] = $result;
+        //return view('currentResult',$data)->render();
+        return view('xsmtResult')->with($data);
+    }
+
+    public function xsmtShow(Request $request,$region){
+        $result = Result::where('lottery_region',$region)->get();
+        return $result;
+    }
 }
