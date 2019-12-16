fd76d091ce9944db53cac250075c09eecde088e0
Sumit Sharma <sumit.sharma@bda.media>
2019-12-16 17:46:05 +0530

    Update issues

--- resources/views/xsmtResult.blade.php
+++ resources/views/xsmtResult.blade.php
@@ -60,7 +60,7 @@
 
             @foreach($comp as $k=>$compn)
                 @php  $end = strlen($compn)-4; if($companyName == substr($compn,2,$end)){ $active="active"; }else{ $active=""; } @endphp
-                <li class="{{ $active }}"><a href="/xsmn/{{ substr($compn,2,$end) }}" title="{{ substr($compn,2,$end) }}">{{ substr($compn,2,$end) }}</a></li>
+                <li class="{{ $active }}"><a href="/xsmt/{{ substr($compn,2,$end) }}" title="{{ substr($compn,2,$end) }}">{{ substr($compn,2,$end) }}</a></li>
             @endforeach
         </ul>
     </div>
@@ -149,14 +149,14 @@
                                 <div class="block-main-content">
                                     <table class="table table-bordered table-striped table-xsmb">
                                         <tbody>
-                                        <tr>
+                                     {{--   <tr>
                                             <td style="width: 15%"> @php $prize_1 = json_decode($printresult->prize_1); @endphp {{ key($prize_1) }}</td>
                                             <td class="text-center">
                                                 @foreach($prize_1->{key($prize_1)} as $k=>$p1)
                                                     <span class=" special-code div-horizontal">{{ $p1 }} </span>
                                                 @endforeach
                                             </td>
-                                        </tr>
+                                        </tr>--}}
 
                                         <tr>
                                             <td>@php $prize_2 = json_decode($printresult->prize_2);   @endphp {{ key($prize_2) }}</td>
