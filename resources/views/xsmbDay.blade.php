1277b454532ded869669d55a31f89e1a35900b0c
Sumit Sharma <sumit.sharma@bda.media>
2019-12-25 17:27:07 +0530

    Update data for xsmb

--- resources/views/xsmbDay.blade.php
+++ resources/views/xsmbDay.blade.php
@@ -11,13 +11,14 @@
 
         <div class="tab" role="tabpanel">
             <ul class="nav-tabs day-selector">
-                <li><a href="/ket-qua-xo-so-mien-trung/kqxsmt-thu-hai" title="XSMB Thứ 2">Thứ hai</a></li>
-                <li><a href="/ket-qua-xo-so-mien-trung/kqxsmt-thu-ba" title="XSMB Thứ 3">Thứ ba</a></li>
-                <li><a href="/ket-qua-xo-so-mien-trung/kqxsmt-thu-tu" title="XSMB Thứ 4">Thứ tư</a></li>
-                <li><a href="/ket-qua-xo-so-mien-trung/kqxsmt-thu-nam" title="XSMB Thứ 5">Thứ năm</a></li>
-                <li><a href="/ket-qua-xo-so-mien-trung/kqxsmt-thu-sau" title="XSMB Thứ 6">Thứ sáu</a></li>
-                <li><a href="/ket-qua-xo-so-mien-trung/kqxsmt-thu-bay" title="XSMB Thứ 7">Thứ bảy</a></li>
-                <li><a href="/ket-qua-xo-so-mien-trung/kqxsmt-chu-nhat" title="XSMB Chủ Nhật">Chủ Nhật</a></li>
+                <li><a href="/ket-qua-xo-so-mien-bac" title="XSMB Thứ 2">Miền Bắc</a></li>
+                <li><a href="/ket-qua-xo-so-mien-bac/kqxsmt-thu-hai" title="XSMB Thứ 2">Thứ hai</a></li>
+                <li><a href="/ket-qua-xo-so-mien-bac/kqxsmt-thu-ba" title="XSMB Thứ 3">Thứ ba</a></li>
+                <li><a href="/ket-qua-xo-so-mien-bac/kqxsmt-thu-tu" title="XSMB Thứ 4">Thứ tư</a></li>
+                <li><a href="/ket-qua-xo-so-mien-bac/kqxsmt-thu-nam" title="XSMB Thứ 5">Thứ năm</a></li>
+                <li><a href="/ket-qua-xo-so-mien-bac/kqxsmt-thu-sau" title="XSMB Thứ 6">Thứ sáu</a></li>
+                <li><a href="/ket-qua-xo-so-mien-bac/kqxsmt-thu-bay" title="XSMB Thứ 7">Thứ bảy</a></li>
+                <li><a href="/ket-qua-xo-so-mien-bac/kqxsmt-chu-nhat" title="XSMB Chủ Nhật">Chủ Nhật</a></li>
             </ul>
         </div>
 
