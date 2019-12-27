4f4e1d2e4e2a6f7efd8873ed088f1d47d8c421de
Kuldeepbda <kuldeep.raj@bda.media>
2019-12-27 17:50:57 +0530

    top table

--- resources/views/todayResult.blade.php
+++ resources/views/todayResult.blade.php
@@ -18,7 +18,7 @@
         </div>
     </div>
     <div id="u118" class="ax_default">
-        <div id="u119" class="ax_default table_cell" style="cursor: pointer;">
+        <div id="u119" class="ax_default table_cell remove-line-height" style="cursor: pointer;">
 
             <div id="u119_text" class="text ">
                 <a href="/{{ "ket-qua-xo-so-mien-bac/" }}" ><span id="cache1" style="">{{ "Miền Bắc" }}</span></a>
@@ -26,7 +26,7 @@
         </div>
         <!-- Unnamed (Table Cell) -->
         @foreach($todayCompanies as $data)
-        <div id="u119" class="ax_default table_cell" style="cursor: pointer;">
+        <div id="u119" class="ax_default table_cell remove-line-height" style="cursor: pointer;">
 
             <div id="u119_text" class="text ">
                  <a href="@if($data->lottery_region == 'XSMN') {{ "/ket-qua-xo-so-mien-nam/kqxsmn-" }}{{ $data->lottery_company_slug }}  @elseif($data->lottery_region == 'XSMT') {{ "/ket-qua-xo-so-mien-trung/kqxsmt-" }}{{ $data->lottery_company_slug }}  @elseif($data->lottery_region == 'XSMB') {{ "ket-qua-xo-so-mien-bac/" }}{{ $data->lottery_company_slug }}  @endif" ><span id="cache1" style="">{{ $data->lottery_company_names }}</span></a>
