ebe1c68939a5b619d79b2fb490fa7aaebd0da6f4
Sumit Sharma <sumitsharma@Sumits-MacBook-Air.local>
2020-01-08 18:37:25 +0530

    pagination issues fix

--- app/Http/Controllers/Results.php
+++ app/Http/Controllers/Results.php
@@ -199,7 +199,7 @@ class Results extends Controller
     public function xsmnIndex(Request $request,$company='XSTG'){
 
 
-        $result = Result::where('lottery_region', 'XSMN')->orderBy('result_day_time', 'desc')->paginate(7);
+        $result = Result::where('lottery_region', 'XSMN')->orderBy('result_day_time', 'desc')->paginate(6);
 
         $t = 0;
         foreach ($result as $res){
@@ -239,7 +239,7 @@ class Results extends Controller
     public function xsmtIndex(Request $request){
 
         //whereBetween
-        $result = Result::where('lottery_region', 'XSMT')->orderBy('result_day_time')->paginate(7);
+        $result = Result::where('lottery_region', 'XSMT')->orderBy('result_day_time')->paginate(6);
 
         $t = 0;
         foreach ($result as $res){
