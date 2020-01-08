6d7a5a06458cf376c40916d80c0b692aecba4df2
Sumit Sharma <sumitsharma@Sumits-MacBook-Air.local>
2020-01-08 17:02:06 +0530

    Update tracking and pagination design

--- app/Http/Controllers/Results.php
+++ app/Http/Controllers/Results.php
@@ -17,9 +17,9 @@ class Results extends Controller
         $orig_date1 = Carbon::createFromFormat("!Y-m-d",$orig_date1->subDay(6)->format("Y-m-d"));
         $count = Result::where('result_day_time' ,'>=', $orig_date)->count();
             if($count > 0){
-                $result = Result::where('result_day_time' ,'>=', $orig_date1)->orderBy('result_day_time', 'desc')->orderBy('lottery_region', 'desc')->get();
+                $result = Result::where('result_day_time' ,'>=', $orig_date1)->orderBy('result_day_time', 'desc')->orderBy('lottery_region', 'asc')->get();
             }else{
-                $result = Result::where('result_day_time' ,'>=', $orig_date1)->orderBy('result_day_time', 'desc')->orderBy('lottery_region', 'desc')->get();
+                $result = Result::where('result_day_time' ,'>=', $orig_date1)->orderBy('result_day_time', 'desc')->orderBy('lottery_region', 'asc')->get();
 
             }
         $new = array();
