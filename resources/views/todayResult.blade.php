81bf45a2c9fb530d6154c872f096cbcf6cbc06af
Kuldeepbda <kuldeep.raj@bda.media>
2020-02-13 10:49:15 +0530

    remove spaces

--- resources/views/todayResult.blade.php
+++ resources/views/todayResult.blade.php
@@ -29,8 +29,7 @@
         <div id="u119" class="ax_default table_cell remove-line-height" style="cursor: pointer;">
 
             <div id="u119_text" class="text ">
-                 <a href="@if($data->lottery_region == 'XSMN') 
-                        {{ "/ket-qua-xsmn/kqxsmn-" }}{{ $data->lottery_company_slug }}
+                 <a href="@if($data->lottery_region == 'XSMN') {{ "/ket-qua-xsmn/kqxsmn-" }}{{ $data->lottery_company_slug }}
                     @elseif($data->lottery_region == 'XSMT') 
                         {{ "/ket-qua-xsmt/kqxsmt-" }}{{ $data->lottery_company_slug }}  
                     @elseif($data->lottery_region == 'XSMB') 
@@ -38,8 +37,7 @@
                     @elseif($data->lottery_region == 'Vietlott') 
                         {{ "/ket-qua-vietlott/kqvietlott-" }}{{ $data->lottery_company_slug }}  
                     @endif">
-
-                     @php if($data->lottery_region == 'Vietlott') { @endphp
+                    @php if($data->lottery_region == 'Vietlott') { @endphp
                         <span id="cache1" style="">{{getVietlottText($data->lottery_company_slug)}}</span>
                     @php } else { @endphp
                         <span id="cache1" style="">{{ $data->lottery_company_names }}</span>
