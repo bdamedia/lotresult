d982007386eec9d81bfb3dbd25b34b82e39235c7
Kuldeepbda <kuldeep.raj@bda.media>
2020-02-25 18:26:00 +0530

    changes files

--- app/Http/Controllers/Crawler.php
+++ app/Http/Controllers/Crawler.php
@@ -380,10 +380,10 @@ class Crawler extends Controller
             $company = getVietlottValueForSideBar($company);
         }
 
-        if(empty($number)) {
-            $result = Result::where('lottery_company', $company)->where('result_day_time','>=',$date1)->where('result_day_time','<',$date2)->get();
-            //$result = Result::where('lottery_company', $company)->get();
-        } else {
+       /* if(empty($number)) {*/
+        $result = Result::where('lottery_company', $company)->where('result_day_time','>=',$date1)->where('result_day_time','<',$date2)->get();
+        //$result = Result::where('lottery_company', $company)->get();
+        /*} else {
             $result = Result::where('lottery_company', $company)
                 ->where('result_day_time','>=',$date1)
                 ->where('result_day_time','<',$date2)
@@ -396,7 +396,7 @@ class Crawler extends Controller
                 ->orWhere('prize_8', 'like', "%{$number}%")
                 ->orWhere('prize_9', 'like', "%{$number}%")
                 ->get();
-        }
+        }*/
 
         if($result->count()) {
             $checkViewRegion = collect($result)->first()->lottery_region;
